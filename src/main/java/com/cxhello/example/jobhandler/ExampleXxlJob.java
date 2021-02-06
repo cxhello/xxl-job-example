0a74afece32007e28b85f0b556a07a957de185a9
cxhello <1061594797@qq.com>
2021-02-06 22:49:03 +0800

    Initial commit

---
+++ src/main/java/com/cxhello/example/jobhandler/ExampleXxlJob.java
@@ -0,0 +1,26 @@
+package com.cxhello.example.jobhandler;
+
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author cxhello
+ * @create 2021/2/6
+ */
+@Component
+public class ExampleXxlJob {
+
+    private static Logger logger = LoggerFactory.getLogger(ExampleXxlJob.class);
+
+    @Value("${server.port}")
+    private int port;
+
+    @XxlJob("testXxlJob")
+    public void testXxlJob() {
+        logger.info("XXL-JOB, Hello World, port: {}.", port);
+    }
+
+}
